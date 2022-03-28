2adbe5d7f2f0cfaec6b1d318b92a6260ad5154a3
Arin0508 <77500921+Arin0508@users.noreply.github.com>
2022-03-28 19:26:59 +0530

    Changed header

--- arithmetic.hpp
+++ arithmetic.hpp
@@ -1,6 +1,10 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+//#include<algorithm>
+#include<iterator>
 using namespace std;
 #define base 256
+typedef long long ll;
 class number
 {
     public:
@@ -10,3 +14,4 @@ class number
 };
 number add(number &a,number&b);
 number sub(number &a,number &b);
+number multiply_karatsuba(number &a,number &b);
