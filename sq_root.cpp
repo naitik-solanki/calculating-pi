8be0a90eb9a2875283d8656f746e065c4f19783b
naitik-solanki <naitiksolanki2002@gmail.com>
2022-03-30 14:57:18 +0530

    changed sq_root

--- sq_root.cpp
+++ sq_root.cpp
@@ -9,7 +9,14 @@ number sq_root(number a)
     while(1)
     {
         number temp = long_Division(a,guess);
+        number prev = guess;
         guess = multiply_Karatsuba(half,add(temp,guess));
-        
+        number temp2= sub(guess,prev);
+        if(temp2.mini_power + temp2.digits.size()<-2)
+        {
+            break;
+        }
+
     }
+    return guess;
 }
